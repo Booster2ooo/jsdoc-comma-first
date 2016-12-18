31288cf233fc98fa175aa691965dded65eb8e20d
Booster2ooo <the._.truth@hotmail.fr>
2016-12-18 09:11:22 +0100

    git creation

---
+++ README.md
@@ -0,0 +1,8 @@
+# JSDoc-Comma-First
+
+## Description
+jsdoc-comma-first is a plugin for [JSDoc](http://usejsdoc.org/) that enable comma first coding style. In comma first style, when declaring variables or properties, instead of putting the commas at the end of the line, we'll put it at the beginning of the next one.
+This seems to be an [issue](https://github.com/jsdoc3/jsdoc/issues/446) in JSDoc so I decided to provide this plugin.
+
+## How to
+Just grab the plugin and [configure JSDoc](http://usejsdoc.org/about-configuring-jsdoc.html#plugins) accordingly.
---
+++ jsdoc-comma-first.js
@@ -0,0 +1,35 @@
+var commentsBuffer = [];
+
+exports.handlers = {
+	beforeParse: function(e) {
+        var source = e.source
+		  , lines = source.split('\n')
+		  , newSource = ''
+		  ;
+		lines.forEach(function(line) {
+			var linecopy = line.trim()
+			  , bufferLength = commentsBuffer.length
+			  ;
+			if(linecopy.indexOf('/**') === 0 || bufferLength) {
+				var prev = commentsBuffer[bufferLength-1];
+				if(prev && prev.indexOf('*/') > -1) {
+					if(linecopy.indexOf(',') === 0) {
+						commentsBuffer.splice(0,0,',');
+						line = line.replace(',','');
+					}
+					commentsBuffer.push(line);
+					newSource += commentsBuffer.join('\n');
+					commentsBuffer = [];
+					
+				}
+				else {
+					commentsBuffer.push(line);
+				}
+			}
+			else {
+				newSource += line+'\n';
+			}
+		});
+        e.source = newSource;
+    }
+};
