166367d463068fa3cc297f2bf27286797c2c03de
Booster2ooo <the._.truth@hotmail.fr>
2018-04-02 10:25:30 +0200

    Fixed indentation (1 tab -> 4 spaces)
    Fixed indentation because tabs are not pretty on Github

--- jsdoc-comma-first.js
+++ jsdoc-comma-first.js
@@ -1,36 +1,36 @@
 var commentsBuffer = [];
 
 exports.handlers = {
-	beforeParse: function(e) {
+    beforeParse: function(e) {
         var source = e.source
-		  , lines = source.split('\n')
-		  , newSource = ''
-		  ;
-		lines.forEach(function(line) {
-			var linecopy = line.trim()
-			  , bufferLength = commentsBuffer.length
-			  ;
-			if(linecopy.indexOf('/**') === 0 || bufferLength) {
-				var prev = commentsBuffer[bufferLength-1];
-				if(prev && prev.indexOf('*/') > -1) {
-					if(linecopy.indexOf(',') === 0) {
-						commentsBuffer.splice(0,0,',');
-						line = line.replace(',',' ');
-					}
-					commentsBuffer.push(line);
-					newSource += commentsBuffer.join('\n') + '\n';
-					commentsBuffer = [];
-					
-				}
-				else {
-					commentsBuffer.push(line);
-				}
-			}
-			else {
-				newSource += line+'\n';
-			}
-		});
-		newSource = newSource.replace(/\n,/g,',');
+          , lines = source.split('\n')
+          , newSource = ''
+          ;
+        lines.forEach(function(line) {
+            var linecopy = line.trim()
+              , bufferLength = commentsBuffer.length
+              ;
+            if(linecopy.indexOf('/**') === 0 || bufferLength) {
+                var prev = commentsBuffer[bufferLength-1];
+                if(prev && prev.indexOf('*/') > -1) {
+                    if(linecopy.indexOf(',') === 0) {
+                        commentsBuffer.splice(0,0,',');
+                        line = line.replace(',',' ');
+                    }
+                    commentsBuffer.push(line);
+                    newSource += commentsBuffer.join('\n') + '\n';
+                    commentsBuffer = [];
+                    
+                }
+                else {
+                    commentsBuffer.push(line);
+                }
+            }
+            else {
+                newSource += line+'\n';
+            }
+        });
+        newSource = newSource.replace(/\n,/g,',');
         e.source = newSource;
     }
 };
